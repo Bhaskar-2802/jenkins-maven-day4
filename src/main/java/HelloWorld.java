363782d58f083fae5996336848c5a838833cfd7f
Bhaskar Reddy <bhaskar.unf207@gmail.com>
2026-08-27 02:28:37 -0400

    add maven java project

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static String message() {
+        return "Hello from Maven and Jenkins!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(message());
+    }
+}
